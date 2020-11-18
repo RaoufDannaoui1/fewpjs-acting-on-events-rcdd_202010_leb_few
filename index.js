689179b8232b889fb34b050803eea049e1190c98
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2020-11-18 14:42:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function moveDodgerRight() {
   let rightNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
 
-  if (left > 0) {
+  if (left < 0) {
     dodger.style.left = `${left + 1}px`;
   }
 }
