d42d5d461ea09a2550527e974c5ef55a5ce57c5b
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2020-11-18 14:42:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function moveDodgerRight() {
   let rightNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
 
-  if (left < 0) {
-    dodger.style.left = `${left + 1}px`;
+  if (left > 0) {
+    dodger.style.right = `${left + 1}px`;
   }
 }
 document.addEventListener("keydown", function(e) {
